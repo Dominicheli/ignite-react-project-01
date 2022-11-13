ef445d16ff84a99efd5e4b233f0385bbc5df7ad1
felipe.dominicheli <felipe.dominicheli@tegra.com.br>
2022-11-13 11:33:06 -0300

    module 1 starting the project with vite and creating the first component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {Post} from './Post'
+
+export function App() {
+
+  return (
+    <Post 
+      author="Felipe Dominicheli" 
+      content="lorem ipsum dolor sit amet, consectetur adip" 
+    />
+  )
+}
