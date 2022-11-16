5afd8448b5e9073f96df7cec616d1c0347992d1e
felipe.dominicheli <felipe.dominicheli@tegra.com.br>
2022-11-16 19:06:41 -0300

    module 2 sidebar creation and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import Header from "./components/Header";
 import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Felipe Dominicheli"
-        content="lorem ipsum dolor sit amet, consectetur adip"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Felipe Dominicheli" content="Lorem ipsum dolor sit" />
+          <Post author="Felipe Dominicheli" content="Lorem ipsum dolor sit" />
+        </main>
+      </div>
     </div>
   );
 }
